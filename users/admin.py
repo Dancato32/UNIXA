f67441d3c6e8f0d4bd0f2258aa3f40ae1efacec9
Daniel cato <danielcato038@gmail.com>
2026-03-17 07:32:19 +0000

    fix: custom user admin registration

--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser, UserAdmin)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'is_student', 'is_staff', 'is_active')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Custom Fields', {'fields': ('is_student', 'profile_picture')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Custom Fields', {'fields': ('is_student', 'profile_picture')}),
+    )
